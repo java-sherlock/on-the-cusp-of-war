b91301fc4c4b0bf491ba654b4c653fcefec2d742
bsevgeny@gmail.com <papakita2009>
2018-10-18 02:37:44 +0300

    solution

--- extension.groovy
+++ extension.groovy
@@ -7,15 +7,40 @@ import javax.annotation.PostConstruct
 
 beans {
     smokingRoom SmokingRoom
-    printerStation PS
+    bpp ConversionService
+//    printеrStation PS
 }
+/*
 class PS extends PrintеrStation{
-    @Override
-    void print(Order order) {
-        order-=/(?s)spring.*?spring/
+    public void print(Order order) {
+
+        order.body-=~/(?s)spring.*?spring/
         super.print(order)
     }
+
 }
+*/
+class ConversionService implements BeanPostProcessor{
+    @Override
+    Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        println beanName
+        if (beanName.equals("printеrStation") || bean.class.name == 'PrinterStation') {
+            println "PrinterStation will be replaced now"
+            return new PrintеrStation(){
+                @Override
+                void print(Order order) {
+                    println "new PrinterStation is working"
+                    order.body-=~/(?s)spring.*?spring/
+                    super.print(order)
+                }
+            }
+        }
+        return bean
+
+    }
+}
+
+
 class SmokingRoom {
     @PostConstruct
     def ventilationSystemOn() {
