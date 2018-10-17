4d1f8163a5864211f54607fd64a6db7dd5e64665
bsevgeny@gmail.com <papakita2009>
2018-10-18 02:21:13 +0300

    solution

--- extension.groovy
+++ extension.groovy
@@ -1,40 +1,47 @@
-import groovy.transform.Canonical
-import org.springframework.beans.BeansException
-import org.springframework.beans.factory.config.BeanPostProcessor
 import org.sherlock.s01.Order
 import org.sherlock.s01.PrintеrStation
+import org.springframework.beans.BeansException
+import org.springframework.beans.factory.config.BeanPostProcessor
 
 import javax.annotation.PostConstruct
 
 beans {
     smokingRoom SmokingRoom
-    conversionServiceBPP ConversionService
+//    bpp ConversionService
+    printеrStation PS
 }
+class PS extends PrintеrStation{
+    public void print(Order order) {
 
-class SmokingRoom {
-    @PostConstruct
-    def ventilationSystemOn(){
-        println 'getting this smoke out!'
+        order.body-=~/(?s)spring.*?spring/
+        super.print(order)
     }
+
 }
-
-@Canonical
-class ConversionService implements BeanPostProcessor {
-
+class ConversionService implements BeanPostProcessor{
     @Override
-    Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (beanName.equals('printеrStation')) {
-            return new PrintеrStation() {
+    Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        println beanName
+        if (beanName.equals("printеrStation") || bean.class.name == 'PrinterStation') {
+            println "PrinterStation will be replaced now"
+            return new PrintеrStation(){
                 @Override
                 void print(Order order) {
-                    String text = order.getBody()
-                    text-=~/(?s)(spring).*?(spring)/
-                    order.setBody(text)
+                    println "new PrinterStation is working"
+                    order.body-=~/(?s)spring.*?spring/
                     super.print(order)
                 }
             }
-        } else {
-            bean
         }
+        return bean
+
+    }
+}
+
+
+class SmokingRoom {
+    @PostConstruct
+    def ventilationSystemOn() {
+        println 'getting this smoke out!'
     }
 }
