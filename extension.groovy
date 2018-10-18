817f473b7a5f8cf46c8c77570f2f5315ff0a0f28
bsevgeny@gmail.com <papakita2009>
2018-10-18 03:08:07 +0300

    solution

--- extension.groovy
+++ extension.groovy
@@ -1,5 +1,5 @@
 import org.sherlock.s01.Order
-import org.sherlock.s01.PrintеrStation
+import org.sherlock.s01.РrintеrStation
 import org.springframework.beans.BeansException
 import org.springframework.beans.factory.config.BeanPostProcessor
 
@@ -7,38 +7,26 @@ import javax.annotation.PostConstruct
 
 beans {
     smokingRoom SmokingRoom
-    bpp ConversionService
-//    printеrStation PS
+    bpp PS
 }
-/*
-class PS extends PrintеrStation{
-    public void print(Order order) {
 
-        order.body-=~/(?s)spring.*?spring/
-        super.print(order)
-    }
-
-}
-*/
-class ConversionService implements BeanPostProcessor{
+class PS implements BeanPostProcessor{
     @Override
     Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         println beanName
-        if (beanName.equals("printеrStation") || bean.class.name == 'PrinterStation') {
-            println "PrinterStation will be replaced now"
-            return new PrintеrStation(){
+        if (beanName == 'рrintеrStation') {
+            return new РrintеrStation(){
                 @Override
                 void print(Order order) {
-                    println "new PrinterStation is working"
                     order.body-=~/(?s)spring.*?spring/
                     super.print(order)
                 }
             }
         }
-        return bean
-
     }
 }
+
+
 
 
 class SmokingRoom {
--- src/main/java/org/sherlock/s01/Secretary.java
+++ src/main/java/org/sherlock/s01/Secretary.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 // меня не подкупишь и на прокси не заменишь
 public final class Secretary {
     private final Typist typist;
-    private final PrintеrStation printer;
+    private final РrintеrStation printer;
 
     @Scheduled(fixedDelay = 100000)
     @SneakyThrows
